7821dd22704b2d6125cd910f6d308171d9ad349e
Erin K. Kennon <ekkennon@gmail.com>
2017-04-04 21:11:03 -0500

    finished part 1

---
+++ src/java/business/Member.java
@@ -0,0 +1,146 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package business;
+
+/**
+ *
+ * @author raefo
+ */
+public class Member {
+    private String memID;
+    private String memDate;
+    private String fname;
+    private String lname;
+    private String midName;
+    private String status;
+    private long password;
+    private long pwAttempt;
+    
+    public Member() {
+        setMemID("");
+        setMemDate("");
+        setFname("");
+        setLname("");
+        setMidName("");
+        setStatus("");
+        setPassword(-1);
+        setPwAttempt(0);
+    }
+
+    /**
+     * @return the memID
+     */
+    public String getMemID() {
+        return memID;
+    }
+
+    /**
+     * @param memID the memID to set
+     */
+    public void setMemID(String memID) {
+        this.memID = memID;
+    }
+
+    /**
+     * @return the memDate
+     */
+    public String getMemDate() {
+        return memDate;
+    }
+
+    /**
+     * @param memDate the memDate to set
+     */
+    public void setMemDate(String memDate) {
+        this.memDate = memDate;
+    }
+
+    /**
+     * @return the fname
+     */
+    public String getFname() {
+        return fname;
+    }
+
+    /**
+     * @param fname the fname to set
+     */
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    /**
+     * @return the lname
+     */
+    public String getLname() {
+        return lname;
+    }
+
+    /**
+     * @param lname the lname to set
+     */
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    /**
+     * @return the midName
+     */
+    public String getMidName() {
+        return midName;
+    }
+
+    /**
+     * @param midName the midName to set
+     */
+    public void setMidName(String midName) {
+        this.midName = midName;
+    }
+
+    /**
+     * @return the status
+     */
+    public String getStatus() {
+        return status;
+    }
+
+    /**
+     * @param status the status to set
+     */
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    /**
+     * @return the password
+     */
+    public long getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(long password) {
+        this.password = password;
+    }
+    
+    /**
+    * @param attempt the attempt to set
+    */
+    public void setPwAttempt(long attempt) {
+        this.pwAttempt = attempt;
+    }
+    
+    public boolean isAuthenticated() {
+        if (this.password > 0) {
+            if (this.password == this.pwAttempt) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ src/java/servlets/ClubLogonServlet.java
@@ -0,0 +1,123 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import business.Member;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author raefo
+ */
+public class ClubLogonServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        String url = "/Logon.jsp";
+        String msg = "";
+        String sql = "";
+        String userId = "";
+        long pwAttempt;
+        Member m;
+        String dbUrl = "jdbc:mysql://localhost:3306/club?useSSL=false";
+        String dbUser = "root";
+        String dbPw = "sesame";
+        
+        try {
+            userId = request.getParameter("userid").trim();
+            pwAttempt = Long.parseLong(request.getParameter("password").trim());
+            
+            Connection conn = DriverManager.getConnection(dbUrl,dbUser,dbPw);
+            Statement s = conn.createStatement();
+            sql = "SELECT * FROM tblMembers WHERE MemId = '" + userId + "'";
+            ResultSet r = s.executeQuery(sql);
+            if (r.next()) {
+                m = new Member();
+                m.setMemID(userId);
+                m.setPwAttempt(pwAttempt);
+                m.setPassword(r.getLong("Password"));
+                if (m.isAuthenticated()) {
+                    msg = "Member Authenticated!<br/>";
+                    //TODO build rest of member object
+                    //TODO set destination to member screen
+                    //TODO set session variables (if needed)
+                } else {
+                    msg = "Member failed to authenticate<br/>";
+                }
+            } else {
+                msg = "User not found in DB<br/>";
+            }
+        } catch (NumberFormatException e) {
+            msg = "Illegal password: "  + e.getMessage() + "<br/>";
+        } catch (SQLException e) {
+            msg = "SQL Error" + e.getMessage() + "<br/>";
+        }
+        request.setAttribute("msg", msg);
+        RequestDispatcher disp = getServletContext().getRequestDispatcher(url);
+        disp.forward(request,response);
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
